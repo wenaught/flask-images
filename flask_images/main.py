9a2163011f7424f576c4d710add49b6e626a02c8
Petr Liakhavets <petr_liakhavets@epam.com>
2020-05-06 15:22:59 +0300

    updated main app to use database helper

--- flask_images/main.py
+++ flask_images/main.py
@@ -1,18 +1,35 @@
-import boto3
 import argparse
 from random import choice
+
+import boto3
 from botocore.exceptions import ClientError, BotoCoreError
 from flask import Flask, send_file, Response, request
 from werkzeug.utils import secure_filename
+
+from flask_images.database_helper import DatabaseHelper
 
 s3 = boto3.client('s3')
 app = Flask('flask_images')
 DEFAULT_PORT = 8080
 
-parser = argparse.ArgumentParser()
+parser_epilog = """To use the script, please provide a JSON configuration file of the following structure:
+{
+    "host": "server-address",
+    "database": "database-name",
+    "user": "user",
+    "password": "password"
+}
+"""
+parser = argparse.ArgumentParser(prog='flask-images',
+                                 description='Access image storage on S3',
+                                 epilog=parser_epilog,
+                                 formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('bucket', type=str, help='S3 bucket name')
+parser.add_argument('config', type=str, help="Configuration file name for database connection")
 parser.add_argument('-p', '--port', type=int, help='Port to run the app on', default=8080)
 args = parser.parse_args()
+
+helper = DatabaseHelper(args.config, app.logger)
 
 
 @app.route('/random', methods=['GET'])
@@ -39,7 +56,11 @@ def get_image(image_name):
 def upload_image():
     try:
         f = request.files['data']
-        s3.upload_fileobj(f, args.bucket, secure_filename(f.filename))
+        file_path = '/tmp/{}'.format(f.filename)
+        f.save(file_path)
+        helper.upload_metadata(file_path)
+        s3.upload_file(file_path, args.bucket, secure_filename(f.filename))
+        return Response(response='File uploaded successfully', status=200)
     except KeyError:
         return Response(response='File should be sent with \'data\' key', status=400)
     except (ClientError, BotoCoreError) as err:
